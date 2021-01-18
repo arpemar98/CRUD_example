41005c8c1fd3571181c4f2bcc0bd1d40d19c8e60
arpemar98 <arpemar98@hotmail.com>
2021-01-18 14:00:50 -0600

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {
+    this.load();
+  }
+
+  load(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  }
+
+}
